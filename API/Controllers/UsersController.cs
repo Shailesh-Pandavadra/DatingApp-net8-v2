0d85e40aac72a3df43d08bdca20b8fd4ed805c83
Shailesh-Pandavadra <Pandavadrashailesh93@outlook.com>
2025-06-30 00:16:23 +0530

    Initial commit: Setup .NET 8 Web API with EF Core and UsersController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+        // return Ok(users);
+        return users;
+    }
+
+    [HttpGet("{id:int}")]   //  /api/users/1
+    public ActionResult<AppUser> GetUsers(int id)
+    {
+        var user = context.Users.Find(id);
+        if (user == null) return NotFound();
+        return user;
+    }
+}
