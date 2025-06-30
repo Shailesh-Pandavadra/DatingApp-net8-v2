217d27e0a4172f117a9d987a3270f47ebd42c834
Shailesh-Pandavadra <Pandavadrashailesh93@outlook.com>
2025-07-01 00:00:20 +0530

    feat(app): use of built-in @for control flow directive to display users instead of using structural directive

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NgFor
-],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
